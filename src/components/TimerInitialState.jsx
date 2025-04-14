5aa59db9a3ca371aaf7f91c2bdcf3ecbde536577
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-04-14 19:33:21 +0530

    progress update in input but need to fix something

--- src/components/TimerInitialState.jsx
+++ src/components/TimerInitialState.jsx
@@ -1,63 +1,115 @@
-import React, { useContext } from 'react'
-import BoxContainer from './BoxContainer'
+import React, { useContext, useState } from "react";
+import BoxContainer from "./BoxContainer";
 import FullscreenSVG from "../svg/FullscreenSVG";
-import Button from './Button'
-import SmallScreenSVG from '../svg/SmallScreenSVG'
-import { GlobalContext } from '../context/Context'
-import SettingButtonInTimer from './SettingButtonInTimer';
-import PlayButtonSVG from './../svg/PlayButtonSVG';
+import Button from "./Button";
+import SmallScreenSVG from "../svg/SmallScreenSVG";
+import { GlobalContext } from "../context/Context";
+import SettingButtonInTimer from "./SettingButtonInTimer";
+import PlayButtonSVG from "./../svg/PlayButtonSVG";
 
 function TimerInitialState() {
-    const {
-      isFullScreen, setTimerOverlay
-    } = useContext(GlobalContext);
+  const { isFullScreen, setTimerOverlay } = useContext(GlobalContext);
+
+  const [hour, setHour] = useState("");
+  const [minute, setMinute] = useState("");
+  const [second, setSecond] = useState("");
 
   return (
     <BoxContainer
-          classes={
-            "h-full flex p-4 items-center flex-col justify-between w-full flex-1 border border-zinc-800"
-          }
-        >
-          <div className="w-full flex gap-1 items-end justify-end">
-          {!isFullScreen ? <FullscreenSVG /> : <SmallScreenSVG/>}
-          </div>
+      classes={
+        "h-full flex p-4 items-center flex-col justify-between w-full flex-1 border border-zinc-800"
+      }
+    >
+      <div className="w-full flex gap-1 items-end justify-end">
+        {!isFullScreen ? <FullscreenSVG /> : <SmallScreenSVG />}
+      </div>
 
-          <div className="w-[307px] sm:w-[340px] items-center justify-center flex flex-col ">
-            <div className="w-full flex flex-col items-center justify-center p-2">
-              <div className="flex items-center justify-center gap-2 w-full">
-                <p>Enter timer Duration Below</p>
-              </div>
-              <div className=" text-5xl sm:text-7xl font-sans font-bold text-zinc-50 flex text-end gap-4 pt-10 pb-6 px-2">
-                <p>00</p>
-                <p>:</p>
-                <p>00</p>
-                <p>:</p>
-                <p>00</p>
-              </div>
-              <div className="flex text-[14px] sm:text-[20px] items-center justify-evenly sm:justify-between sm:gap-4 w-full">
-                <p>Hour</p>
-                <p>Minute</p>
-                <p>Second</p>
-              </div>
-              <div className="mt-3 flex items-center gap-2 justify-center">
-                <SettingButtonInTimer
-                onClick={() => setTimerOverlay(true)}
+      <div className="w-[315px] sm:w-[360px] items-center justify-center flex flex-col ">
+        <div className="w-full flex flex-col items-center justify-center p-2">
+          <div className="flex items-center justify-center gap-2 w-full">
+            <p>Enter timer Duration Below</p>
+          </div>
+          <div class="flex text-4xl text-white sm:text-7xl my-4 flex-row items-center justify-center gap-0 sm:my-8 sm:gap-4">
+            <div class="flex max-w-[80px] flex-col items-center sm:max-w-[100px]">
+              <div class="relative">
+                <input
+                  type="text"
+                  className="border-none bg-transparent text-center placeholder-white outline-none  w-full pb-7 font-bold sm:pb-9"
+                  value={hour !== "" ? hour.padStart(2,'0') : ""}
+                  placeholder="00"
+                  maxLength={2}
+                  onChange={(e) => {
+                    const val = e.target.value;
+                    if (/^\d{0,2}$/.test(val)) {
+                      setHour(val);
+                    }
+                  }}
                 />
-                <Button
-                    text={"Start Timer"}
-                    classes={"bg-zinc-800"}
-                    isPlayButton={true}
-                    svg={<PlayButtonSVG />}
-                  />
+                <span class="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 text-base text-zinc-500 sm:text-2xl">
+                  hour
+                </span>
+              </div>
+            </div>
+            <div class="relative -top-4 sm:-top-6">:</div>
+            <div class="flex max-w-[100px] flex-col items-center sm:max-w-[100px]">
+              <div class="relative">
+                <input
+                  type="text"
+                  className="border-none bg-transparent text-center placeholder-white  w-full pb-7 font-bold sm:pb-9"
+                  value={minute !== "" ? minute.padStart(2,'0') : ""}
+                  placeholder="00"
+                  maxLength={2}
+                  onChange={(e) => {
+                    const val = e.target.value;
+                    if (/^\d{0,2}$/.test(val)) {
+                      setMinute(val);
+                    }
+                  }}
+                />
+                <span class="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 text-base text-zinc-500 sm:text-2xl">
+                  min
+                </span>
+              </div>
+            </div>
+            <div class="relative -top-4 sm:-top-6">:</div>
+            <div class="flex max-w-[80px] flex-col items-center sm:max-w-[100px]">
+              <div class="relative">
+                <input
+                  type="text"
+                  className="border-none bg-transparent text-center placeholder-white outline-none w-full pb-7 font-bold sm:pb-9"
+                  value={second !== "" ? second.padStart(2,'0') : ""}
+                  placeholder="00"
+                  maxLength={2}
+                  onChange={(e) => {
+                    const val = e.target.value;
+                    if (/^\d{0,2}$/.test(val)) {
+                      setSecond(val);
+                    }
+                  }}
+                />
+                <span class="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 text-base text-zinc-500 sm:text-2xl">
+                  sec
+                </span>
               </div>
             </div>
           </div>
-
-          <div>
-            <p className="text-zinc-700">No sessions today</p>
+          <div className=" flex items-center gap-2 justify-center">
+            <SettingButtonInTimer onClick={() => setTimerOverlay(true)} />
+            <Button
+              text={"Start Timer"}
+              classes={"bg-zinc-800"}
+              isPlayButton={true}
+              svg={<PlayButtonSVG />}
+            />
           </div>
-        </BoxContainer>
-  )
+        </div>
+      </div>
+
+      <div>
+        <p className="text-zinc-700">No sessions today</p>
+      </div>
+    </BoxContainer>
+  );
 }
 
-export default TimerInitialState
+export default TimerInitialState;
